c1bca82cc94ead2c8594de0313aed9e5bd6a42c8
kimuzzu94 <kimuzzu@gmail.com>
2020-03-05 17:18:02 +0900

    Initial commit

---
+++ src/main/java/org/kimuzzu/toby/testwebservice02/Testwebservice02Application.java
@@ -0,0 +1,13 @@
+package org.kimuzzu.toby.testwebservice02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testwebservice02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Testwebservice02Application.class, args);
+    }
+
+}
